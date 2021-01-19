9ade4256cdf6a4c8a54a2cdba5314dbba3d8b78c
EdrianNewtech <edrian.newtech@newtechs.com.br>
2021-01-19 19:22:08 -0300

    Exemplo de input interno

---
+++ aula-angular/src/app/exemplos-angular/exemplos-angular.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-angular',
+  templateUrl: './exemplos-angular.component.html',
+  styleUrls: ['./exemplos-angular.component.css']
+})
+export class ExemplosAngularComponent {
+  nomeAluno = 'Edrian';  
+  turma     = 'Angular';
+
+  atualizaTurma(input: any){
+    this.turma = input.value;
+  }
+}
+
