8520d3cca6634641ee3bb7fd991d7feb8cf694e3
EdrianNewtech <edrian.newtech@newtechs.com.br>
2021-01-22 20:53:35 -0300

    Aula po-ui Clientes

---
+++ front-dwc/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule, PoModule, HttpClientModule
+  ],
+  exports:  [
+    PoModule, HttpClientModule
+  ],
+})
+export class SharedModule { }
